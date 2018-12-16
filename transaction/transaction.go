8d1320332662cd9e7bb5ba39eb330641db0693c4
root <rnOOBaxbC1>
2018-12-16 07:13:36 +0200

    ToRaw Transaction

--- transaction/transaction.go
+++ transaction/transaction.go
@@ -1,30 +1,34 @@
 package transaction
 
 import (
+	"crypto/sha256"
+	"encoding/binary"
 	"encoding/hex"
 	"log"
 )
 
 type Transaction struct {
 	TxHash    []byte
-	Block     int32
+	Block     uint32
+	NoInBlock uint32
 	Sender    []byte
 	Receiver  []byte
-	Amount    int64
-	Fee       int64
-	Timestamp int32
+	Amount    uint64
+	Fee       uint64
+	Timestamp uint32
 	PublicKey []byte
 	Signature []byte
 }
 
 type TransactionJSON struct {
 	TxHash    string
-	Block     int32
+	Block     uint32
+	NoInBlock uint32
 	Sender    string
 	Receiver  string
-	Amount    int64
-	Fee       int64
-	Timestamp int32
+	Amount    uint64
+	Fee       uint64
+	Timestamp uint32
 	PublicKey string
 	Signature string
 }
@@ -66,5 +70,31 @@ func FromJSON(json TransactionJSON) Transaction {
 		Timestamp: json.Timestamp,
 		PublicKey: PublicKey,
 		Signature: Signature,
+	}
+}
+
+func ToRaw(transaction Transaction) TransactionRaw {
+
+	amount := make([]byte, 8)
+	binary.BigEndian.PutUint64(amount, transaction.Amount)
+
+	fee := make([]byte, 8)
+	binary.BigEndian.PutUint64(fee, transaction.Fee)
+
+	timestamp := make([]byte, 4)
+	binary.BigEndian.PutUint32(timestamp, transaction.Timestamp)
+
+	data := append(transaction.Sender, transaction.Receiver...)
+	data = append(data, amount...)
+	data = append(data, fee...)
+	data = append(data, timestamp...)
+
+	txHash := sha256.Sum256(data)
+
+	return TransactionRaw{
+		TxHash:    txHash[:],
+		TxData:    data,
+		PublicKey: transaction.PublicKey,
+		Signature: transaction.Signature,
 	}
 }
--- transaction/transaction_test.go
+++ transaction/transaction_test.go
@@ -5,25 +5,46 @@ import (
 	"encoding/hex"
 	"fmt"
 	"testing"
+	"time"
 )
 
 func TestFromJSON(t *testing.T) {
+
+	json := GetNewTransaction()
+
+	tx := FromJSON(json)
+
+	fmt.Print(tx)
+
+	fmt.Println("Sender len: %n", len(tx.Sender))
+	fmt.Println("Receiver len: %n", len(tx.Receiver))
+	fmt.Println("PublicKey len: %n", len(tx.PublicKey))
+	fmt.Println("Signature len: %n", len(tx.Signature))
+
+}
+
+func TestToRaw(t *testing.T) {
+	tx := FromJSON(GetNewTransaction())
+
+	raw := ToRaw(tx)
+
+	fmt.Printf("raw: %x\n", raw)
+}
+
+func GetNewTransaction() TransactionJSON {
 
 	walletSender := wallet.CreateWallet()
 	walletReceiver := wallet.CreateWallet()
 
 	pubKey := hex.EncodeToString(walletSender.PublicKey)
 
-	json := TransactionJSON{
+	return TransactionJSON{
 		Sender:    walletSender.Address,
 		Receiver:  walletReceiver.Address,
 		Amount:    1234,
 		Fee:       12,
+		Timestamp: uint32(time.Now().Unix()),
 		PublicKey: pubKey,
 		Signature: "2b4a9f00",
 	}
-
-	tx := FromJSON(json)
-
-	fmt.Print(tx)
 }
