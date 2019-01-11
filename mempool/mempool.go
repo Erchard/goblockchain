d433527825d5e440afb994c93e4ab2fce01620c4
Arsen <arsenguzhva@gmail.com>
2019-01-11 12:40:28 +0200

    Get Transaction from mempool

--- mempool/mempool.go
+++ mempool/mempool.go
@@ -3,3 +3,17 @@ package mempool
 import "../transaction"
 
 var Mempool = make(map[string]transaction.Transaction)
+
+func GetTransactions() []transaction.Transaction {
+
+	var result = make([]transaction.Transaction, 10)
+	i := 0
+	for _, v := range Mempool {
+		result = append(result, v)
+		i++
+		if i >= 10 {
+			break
+		}
+	}
+	return result
+}
